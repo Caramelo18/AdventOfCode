9f7a5cb048de260faff70ff34bb1ae30a316b38a
Caramelo18 <fabiocaramelo18@gmail.com>
2019-12-02 00:58:21 +0100

    structure and day1 part1 added

---
+++ lib/adventofcode.ex
@@ -0,0 +1,27 @@
+Code.require_file("day1/part1.ex")
+defmodule Adventofcode do
+  def read_input(name) do
+    case File.read("../assets/" <> name) do
+      {:ok, content} ->
+        String.split(content, ~r/\R/)
+        |> Enum.reverse() |> tl() |> Enum.reverse()
+        |> Enum.map(fn n -> Integer.parse(n) |> elem(0) end)
+      {:error, reason} ->
+        IO.puts(reason)
+    end
+  end
+
+  def write_solution(content, filename) do
+    case File.open("../assets/" <> filename, [:write]) do
+      {:ok, file} ->
+        content
+        |> Enum.each(fn(x) -> IO.write(file, Integer.to_string(x) <> "\n") end)
+        File.close(file)
+    end
+  end
+end
+
+Adventofcode.read_input("d1p1")
+|> Enum.map(fn n -> Day1.part1(n) end)
+|> Enum.sum()
+|> IO.puts()
---
+++ lib/day1/part1.ex
@@ -0,0 +1,6 @@
+defmodule Day1 do
+  def part1(arg) do
+    div(arg, 3)
+    |> Kernel.-(2)
+  end
+end
